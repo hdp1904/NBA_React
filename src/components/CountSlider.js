f4daba48c5d90cf5a20eed25e8cde14169228623
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2021-02-05 08:35:01 +0800

    Update CountSlider.js

--- src/components/CountSlider.js
+++ src/components/CountSlider.js
@@ -24,7 +24,7 @@ export class CountSlider extends React.Component {
          <InputNumber
            min={1}
            max={20}
-           style={{ marginLeft: 18 }}
+           style={{ marginLeft: 19 }}
            value={this.state.inputValue}
            onChange={this.onChange}
          />
