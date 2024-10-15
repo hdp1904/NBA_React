d88a7782f608d28bbe873adb782c965a0f28ae82
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2024-10-15 08:34:13 -0700

    Update TopNavBar.js

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -6,7 +6,7 @@ export class TopNavBar extends React.Component {
  render() {
    return (
      <header className="App-header">
-   
+       <img src={logo} className="App-logo" alt="App-logo" />
        <a href="https://github.com/hdp1904/NBA-react" id="header-word">GitHub</a>
        <a href="https://www.linkedin.com/in/markycchen/" id="header-word">About Me</a>
      </header>
