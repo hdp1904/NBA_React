c94c1872957fb082df8ba288a4081f7adfbf747a
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2024-12-26 04:42:23 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
 
 serviceWorker.unregister();
