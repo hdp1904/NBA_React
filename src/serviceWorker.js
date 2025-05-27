8a581ad857f4936d79d00af12d4f3b0f7364238b
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2025-05-28 00:30:18 +0900

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,7 +1,7 @@
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     window.location.hostname === '[::1]' ||
-    window.location.hostname.match(
+
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
