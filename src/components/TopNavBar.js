452e71c954b4c03a8019c79823ef80cb3fbb0bf1
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2026-05-04 08:44:26 -0700

    Update TopNavBar.js

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,15 +1,1 @@
 
-import React from 'react';
-import logo from '../assets/images/nba-logoman-word-white.svg';
-
-export class TopNavBar extends React.Component {
- render() {
-   return (
-     <header className="App-header">
-       <img src={logo} className="App-logo" alt="App-logo" />
-       <a href="https://github.com/hdp1904/NBA-react" id="header-word">GitHub</a>
-       <a href="https://www.linkedin.com/in/markycchen/" id="header-word">About Me</a>
-     </header>
-   );
- }
-}
