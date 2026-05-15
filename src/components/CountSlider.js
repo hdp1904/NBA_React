02fc30aaa3ec7db25fc0332f654f26d36d788395
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2026-05-15 09:49:25 -0700

    Update CountSlider.js

--- src/components/CountSlider.js
+++ src/components/CountSlider.js
@@ -1,1 +1,37 @@
+import React from 'react';
+import { Slider, InputNumber, Row, Col } from 'antd';
+
+export class CountSlider extends React.Component {
+ state = {
+   inputValue: 1,
+ }
+
+ onChange = (value) => {
+   const cleanValue = Number(value) ? value : this.state.inputValue;
+   this.setState({
+     inputValue: cleanValue,
+   });
+   this.props.onCountSliderChange(cleanValue);
+ }
+
+ render() {
+   return (
+     <Row>
+       <Col span={12}>
+         <Slider min={1} max={25} onChange={this.onChange} value={this.state.inputValue} />
+       </Col>
+       <Col span={5}>
+         <InputNumber
+           min={1}
+           max={20}
+           style={{ marginLeft: 12 }}
+           value={this.state.inputValue}
+           onChange={this.onChange}
+         />
+       </Col>
+     </Row>
+   );
+ }
+}
+
 
